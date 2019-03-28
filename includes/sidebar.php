62d97b218cae8b109c17dbed699628211e15a0c8
Đào Duy Khánh <khanhduydao97@gmail.com>
2019-03-28 11:17:21 +0700

    Duy Khanh - Khoi Tao Giao Dien Trang Chu

---
+++ includes/sidebar.php
@@ -0,0 +1,11 @@
+<div class="profile_nav">
+          <ul>
+            <li><a href="profile.php">Hồ sơ</a></li>
+              <li><a href="update-password.php">Đổi mật khẩu</a></li>
+            <li><a href="my-booking.php">Lịch sử đặt xe</a></li>
+            <li><a href="post-testimonial.php">Chứng thực</a></li>
+               <li><a href="my-testimonials.php">Lời chứng thực</a></li>
+            <li><a href="logout.php">Đăng xuất</a></li>
+          </ul>
+        </div>
+      </div>
