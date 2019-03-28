d865e02da13a37ca3ac978b483599aaf72334deb
vinhnguyen72 <vinh7297@gmail.com>
2019-03-28 19:40:47 +0700

    Vinh - Trang Admin

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,31 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Bảng điều khiển</a></li>
+			
+<li><a href="#"><i class="fa fa-files-o"></i> Thương hiệu</a>
+<ul>
+<li><a href="create-brand.php">Tạo thương hiệu</a></li>
+<li><a href="manage-brands.php">Quản lý thương hiệu</a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-sitemap"></i> Phương tiện</a>
+					<ul>
+						<li><a href="post-avehical.php">Đăng một phương tiện</a></li>
+						<li><a href="manage-vehicles.php">Quản lý phương tiện</a></li>
+					</ul>
+				</li>
+				<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Quản lý đặt xe</a></li>
+
+				<li><a href="testimonials.php"><i class="fa fa-table"></i> Quản lý chứng thực</a></li>
+				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop"></i> Quản lý thông tin liên hệ</a></li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> Quản lý người dùng</a></li>
+			<li><a href="manage-pages.php"><i class="fa fa-files-o"></i> Quản lý trang</a></li>
+			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Cập nhật thông tin liên hệ</a></li>
+
+			<li><a href="manage-subscribers.php"><i class="fa fa-table"></i> Quản lý người theo dõi</a></li>
+
+			</ul>
+		</nav>
